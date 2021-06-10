6da79f93f509761c15204f9e1264155041ba9f87
jpe <patriot.game@gmail.com>
2021-06-10 11:26:22 -0400

    tmux launch

--- tmux.sh
+++ tmux.sh
@@ -7,14 +7,13 @@ tmux has-session -t $SN &>/dev/null
 if [ $? != 0 ] 
 then
     tmux new -s $SN -n editor -d -x- -y-
-    tmux splitw -h -p 20 # split vertically by 50%
-    tmux select-pane -t 1
-    tmux splitw -v
-    tmux new-window -t $SN:1 -n dotfiles
-    tmux new-window -t $SN:2 -n irc
+#   tmux splitw -h -p 25 # split vertically by 50%
+#    tmux select-pane -t 1
+#    tmux splitw -v
+    tmux new-window -t $SN:1 -n reference
+    tmux new-window -t $SN:2 -n dotfiles
     tmux select-window -t 0
     tmux select-pane -t 0
-    tmux send-keys "nvim ~/.tmux.conf" C-m 
 
 fi
 
