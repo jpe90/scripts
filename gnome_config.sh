6c5b9cfb8889a61c30012f161a3e59f243a73fe2
jpe <patriot.game@gmail.com>
2021-06-19 10:59:44 -0400

    fix gnome

--- gnome_config.sh
+++ gnome_config.sh
@@ -2,12 +2,12 @@
 
 #!/bin/bash
 
-dconf write /org/gnome/desktop/wm/keybindings/switch-applications "[]"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "'[]'"
 
-dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "[]"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "'[]'"
 
-dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "'[<Alt>Tab]'"
 
-dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift><Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "'[<Shift><Alt>Tab]'"
 
-dconf write /org/gnome/desktop/wm/keybindings/show-desktop "['<Super>d']"
+dconf write /org/gnome/desktop/wm/keybindings/show-desktop "'[<Super>d]'"
--- tmux.sh
+++ tmux.sh
@@ -4,15 +4,21 @@ SN="dev"
 
 tmux has-session -t $SN &>/dev/null
 
+# enhancement: param to specify num of splits
+
 if [ $? != 0 ] 
 then
-    tmux new -s $SN -n editor -d -x- -y-
-#   tmux splitw -h -p 25 # split vertically by 50%
-#    tmux select-pane -t 1
-#    tmux splitw -v
-    tmux new-window -t $SN:1 -n reference
-    tmux new-window -t $SN:2 -n dotfiles
-    tmux select-window -t 0
+    tmux new -s $SN -n editor -d -x- -y- 
+    # tmux splitw -h -p 25 # split vertically by 50%
+    # tmux select-pane -t 1
+    # tmux splitw -v
+    tmux new-window -t $SN:2 -n reference
+    tmux select-window -t 2
+    # tmux splitw -h -p 25 # split vertically by 50%
+    # tmux select-pane -t 2
+    # tmux splitw -v
+    tmux new-window -t $SN:3 -n dotfiles
+    tmux select-window -t 1
     tmux select-pane -t 0
 
 fi
